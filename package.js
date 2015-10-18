8ae766dde93a8dedb2b0d6892d60135b97026eea
Abigail Watson <awatson1978@gmail.com>
2015-10-17 23:12:01 -0700

    bump to 0.2.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "photonic:glass-ui",
-  version: "0.2.4",
+  version: "0.2.5",
   // Brief, one-line summary of the package.
   summary: "Inspired by the Day Made of Glass videos....",
   // URL to the Git repository containing the source code for this package.
