69ca35ea4923c68f8827a52f57120d5ecf587d63
Abigail Watson <awatson1978@gmail.com>
2018-02-11 13:43:34 -0600

    Glass.js

--- index.jsx
+++ index.jsx
@@ -2,6 +2,6 @@ import GlassCard from './client/react/GlassCard.jsx';
 import VerticalCanvas from './client/react/VerticalCanvas.jsx';
 import FullPageCanvas from './client/react/FullPageCanvas.jsx';
 
-import Glass from './client/lib/Glass.js';
+import { Glass } from './client/lib/Glass.js';
 
 export { GlassCard, VerticalCanvas, FullPageCanvas, Glass };
