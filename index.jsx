4c51243097da0f5c54e13f799c2995e2129a7592
Abigail Watson <awatson1978@gmail.com>
2018-02-11 13:08:44 -0600

    Glass.js

--- index.jsx
+++ index.jsx
@@ -2,4 +2,6 @@ import GlassCard from './client/react/GlassCard.jsx';
 import VerticalCanvas from './client/react/VerticalCanvas.jsx';
 import FullPageCanvas from './client/react/FullPageCanvas.jsx';
 
-export { GlassCard, VerticalCanvas, FullPageCanvas };
+import Glass from './client/lib/Glass.js';
+
+export { GlassCard, VerticalCanvas, FullPageCanvas, Glass };
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "clinical:glass-ui",
-  version: "2.0.3",
+  version: "2.1.0",
   summary: "Inspired by the Day Made of Glass videos....",
   git: "http://github.com/clinical-meteor/clinical-glass-ui",
   documentation: "README.md"
