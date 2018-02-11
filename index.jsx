b8aa9d20c768ab138f677cc5f9d14c659c4824ba
Abigail Watson <awatson1978@gmail.com>
2018-02-11 12:02:16 -0600

    FullPageCanvas

--- index.jsx
+++ index.jsx
@@ -1,4 +1,5 @@
 import GlassCard from './client/react/GlassCard.jsx';
 import VerticalCanvas from './client/react/VerticalCanvas.jsx';
+import FullPageCanvas from './client/react/FullPageCanvas.jsx';
 
-export { GlassCard, VerticalCanvas };
+export { GlassCard, VerticalCanvas, FullPageCanvas };
