946245e45bc1a547a1d86030edee95f5b039aa98
Abigail Watson <awatson1978@gmail.com>
2018-02-12 14:15:24 -0600

    DynamicSpacer

---
+++ client/react/DynamicSpacer.jsx
@@ -0,0 +1,34 @@
+import React  from 'react';
+import ReactMixin from 'react-mixin';
+import { ReactMeteorData } from 'meteor/react-meteor-data';
+
+import { Session } from 'meteor/session';
+
+export class DynamicSpacer extends React.Component {
+  getMeteorData() {
+    let data = {
+      style: {
+        // height: '0rem',
+        height: '1px',
+        'WebkitTransition': 'ease .2s',
+        'transition': 'ease .2s'
+      }
+    };
+
+    if (Session.get('hasPageVerticalPadding')) {
+      data.style.height = '3.2rem';
+    }
+
+    return data;
+  }
+
+  render () {
+    return(
+      <div className="spacer" style={this.data.style}></div>
+    );
+  }
+}
+
+ReactMixin(DynamicSpacer.prototype, ReactMeteorData);
+
+export default DynamicSpacer;
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "clinical:glass-ui",
-  version: "2.1.4",
+  version: "2.1.5",
   summary: "Inspired by the Day Made of Glass videos....",
   git: "http://github.com/clinical-meteor/clinical-glass-ui",
   documentation: "README.md"
@@ -17,30 +17,6 @@ Package.onUse(function (api) {
   api.use('react-meteor-data@0.2.15');
   api.use('session');
 
-  // api.use("grove:less@0.1.1");
-  // api.use("templating");
-
-  // api.addFiles('client/components/recordCore/active-record-core.js', ['client']);
-  // api.addFiles('client/components/recordCore/active-record-core.less', ['client']);
-
-  // api.addFiles('client/components/recordHeader/recordHeader.html', ['client']);
-  // api.addFiles('client/components/recordHeader/recordHeader.js', ['client']);
-  // api.addFiles('client/components/recordHeader/recordHeader.less', ['client']);
-
-  // api.addFiles('client/components/recordFooter/recordFooter.html', ['client']);
-  // api.addFiles('client/components/recordFooter/recordFooter.js', ['client']);
-  // api.addFiles('client/components/recordFooter/recordFooter.less', ['client']);
-
-  // api.addFiles("client/glass-ui.js", 'client');
-  // api.addFiles("client/glass-ui.less", 'client');
-  // api.addFiles("client/form.less", 'client');
-
-  // api.addFiles("client/theme.helpers.js", 'client');
-  // api.addFiles("client/themes.global.less", 'client');
-
-  // api.export('recordFooter');
-  // api.export('recordHeader');
-
   api.mainModule('index.jsx', 'client');
 });
 
@@ -49,8 +25,3 @@ Package.onTest(function (api) {
   api.use("clinical:glass-ui");
 });
 
-
-// Npm.depends({
-//   "material-ui": "0.20.0",
-//   "lodash": "4.17.4"
-// });
