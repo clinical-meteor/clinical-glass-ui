c1693411c6c4e3aa12031291a8c2769def8f30c0
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-05-05 16:34:07 -0500

    noDataPadding

--- client/react/TableNoData.jsx
+++ client/react/TableNoData.jsx
@@ -1,7 +1,7 @@
 import React  from 'react';
 import ReactMixin from 'react-mixin';
 import { ReactMeteorData } from 'meteor/react-meteor-data';
-
+import { get } from 'lodash';
 
 export class TableNoData extends React.Component {
   getMeteorData() {
@@ -12,7 +12,12 @@ export class TableNoData extends React.Component {
 
   render () {
     return(
-        <div style={{width: '100%', paddingTop: '120px', textAlign: 'center'}} >
+        <div style={{
+          width: '100%', 
+          paddingTop: get(this, 'props.noDataPadding', 0) + 'px', 
+          paddingBottom: get(this, 'props.noDataPadding', 0) + 'px', 
+          textAlign: 'center'
+          }} >
             <h3>No data.</h3>
             <span>Are you sure you're logged in?</span><br/>
             <span>Do you have an access token?</span><br/>
